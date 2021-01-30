d55c987ab094d5f297c266df2ed1bbc1df0a20ce
Tan Le <tanle@Tans-MacBook-Pro.local>
2021-01-30 16:07:09 -0500

    ch2 - example of observable, subscriber, disposable with demo of running threads on background and UI threads

--- app/src/main/java/packt/reactivestocks/MainActivity.java
+++ app/src/main/java/packt/reactivestocks/MainActivity.java
@@ -1,6 +1,4 @@
 package packt.reactivestocks;
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,12 +7,23 @@ import android.util.Log;
 import android.widget.TextView;
 
 import java.util.Date;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Completable;
+import io.reactivex.CompletableObserver;
+import io.reactivex.CompletableSource;
+import io.reactivex.Maybe;
 import io.reactivex.Observable;
+import io.reactivex.Single;
+import io.reactivex.functions.Action;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.PublishSubject;
 
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.hello_world_salute)
@@ -25,17 +34,145 @@ public class MainActivity extends AppCompatActivity {
     private LinearLayoutManager layoutManager;
     private StockDataAdapter stockDataAdapter;
 
-    @SuppressLint("CheckResult")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        //demo();
+        demo1();
+//        demo2();
+//        demo3();
+//        demo4();
+//        demo5();
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        stockDataAdapter = new StockDataAdapter();
+        recyclerView.setAdapter(stockDataAdapter);
+
+        Observable.just("Please use this app responsibly!")
+                .subscribe(s -> helloText.setText(s));
+
+        Observable.just(
+                new StockUpdate("GOOGLE", 12.43, new Date()),
+                new StockUpdate("APPL", 645.1, new Date()),
+                new StockUpdate("TWTR", 1.43, new Date())
+        )
+                .subscribe(stockUpdate -> {
+                    Log.d("APP", "New update " + stockUpdate.getStockSymbol());
+                    stockDataAdapter.add(stockUpdate);
+                });
+    }
+
+    private void demo5() {
+        Completable completable = Completable.fromAction(() -> {
+            log("Let's do something");
+        });
+
+        completable.subscribe(() -> {
+            log("Finished");
+        }, throwable -> {
+            log(throwable);
+        });
+
+
+        Single.just("One item")
+                .subscribe((item) -> {
+                    log(item);
+                }, (throwable) -> {
+                    log(throwable);
+                });
+
+        Maybe.empty();
+        Maybe.just("Item")
+                .subscribe(s -> {
+                    log("On Success: " + s);
+                });
+
+        Maybe.just("Item")
+                .subscribe(s -> {
+                    log("On Success: " + s);
+                }, throwable -> log("error"));
+
+        Maybe.just("Item")
+                .subscribe(
+                        s -> log("success: " + s),
+                        throwable -> log("error"),
+                        () -> log("onComplete")
+                );
+    }
+
+    private void demo4() {
+        PublishSubject<Integer> observable = PublishSubject.create();
+
+        observable.toFlowable(BackpressureStrategy.MISSING)
+                .buffer(10)
+                .observeOn(Schedulers.computation())
+                .subscribe(v -> log("s", v.toString()), this::log);
+
+        for (int i = 0; i < 1000000; i++) {
+            observable.onNext(i);
+        }
+    }
+
+    private void log(Throwable throwable) {
+        Log.e("APP", "Error", throwable);
+    }
+
+    private void demo3() {
+        Observable.just("One", "Two").toFlowable(BackpressureStrategy.DROP).onBackpressureDrop().buffer(10);
+
+    }
+
+    private void demo2() {
+        Observable.just("One", "Two")
+                .doOnDispose(() -> log("doOnDispose"))
+                .doOnComplete(() -> log("doOnComplete"))
+                .doOnNext(e -> log("doOnNext", e))
+                .doOnEach(e -> log("doOnEach"))
+                .doOnSubscribe((e) -> log("doOnSubscribe"))
+                .doOnTerminate(() -> log("doOnTerminate"))
+                .doFinally(() -> log("doFinally"))
+                .subscribe(e -> log("subscribe", e));
+    }
+
+    private void log(String stage, String item) {
+        Log.d("APP", stage + ":" + Thread.currentThread().getName() + ":" + item);
+    }
+
+    private void log(String stage) {
+        Log.d("APP", stage + ":" + Thread.currentThread().getName());
+    }
+
+    public void demo1() {
+        Observable.just("First item", "Second item")
+                .subscribeOn(Schedulers.io())
+                .doOnNext(e -> Log.d("APP", "on-next:" + Thread.currentThread().getName() + ":" + e))
+                .subscribe(e -> Log.d("APP", "subscribe:" + Thread.currentThread().getName() + ":" + e));
+
+//        Observable.just("First item", "Second item")
+//                .doOnNext(e -> Log.d("APP", "on-next:" + Thread.currentThread().getName() + ":" + e))
+//                .subscribeOn(Schedulers.computation())
+//                .subscribe(e -> Log.d("APP", "subscribe:" + Thread.currentThread().getName() + ":" + e));
+    }
+
     public void demo() {
 
-        Observable.just("1").subscribe(new Consumer<String>() {
+        Observable.just("1")
+                .subscribe(new Consumer<String>() {
                     @Override
                     public void accept(String e) {
                         Log.d("APP", "Hello " + e);
                     }
                 });
 
-        Observable.just("1").map(new Function<String, String>() {
+        Observable.just("1")
+                .map(new Function<String, String>() {
                     @Override
                     public String apply(String s) {
                         return s + "mapped";
@@ -65,7 +202,6 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-        // todo this is the best scenario of observable flow
         Observable.just("1")
                 .map(s -> s + "mapped")
                 .flatMap(s -> Observable.just("flat-" + s))
@@ -73,40 +209,8 @@ public class MainActivity extends AppCompatActivity {
                 .subscribe(e -> Log.d("APP", "Hello " + e),
                         throwable -> Log.d("APP", "Error!"));
 
-        Observable.just("1").subscribe(e -> Log.d("APP", "Hello " + e));
+        Observable.just("1")
+                .subscribe(e -> Log.d("APP", "Hello " + e));
     }
-
-    @SuppressLint("CheckResult")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        ButterKnife.bind(this);
-
-        demo();
-
-        recyclerView.setHasFixedSize(true);
-
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
-        stockDataAdapter = new StockDataAdapter();
-        recyclerView.setAdapter(stockDataAdapter);
-
-        Observable.just("Please use this app responsibly!")
-                .subscribe(s -> helloText.setText(s));
-
-        Observable.just(
-                new StockUpdate("GOOGLE", 12.43, new Date()),
-                new StockUpdate("APPL", 645.1, new Date()),
-                new StockUpdate("TWTR", 1.43, new Date()),
-                new StockUpdate("GME", 325.0, new Date())
-        ).subscribe(stockUpdate -> {
-                    Log.d("APP", "New update " + stockUpdate.getStockSymbol());
-                    stockDataAdapter.add(stockUpdate);
-                });
-    }
-
 
 }
